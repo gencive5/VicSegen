7403a73e3be6185d44e36b1cfd2e9aa8ad1dc867
gencive5 <vic.segen@gmail.com>
2025-03-10 00:38:23 +0100

    buttons layout

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,8 @@ function App() {
   />
 </div>
 
-      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-100 inline-flex gap-4">
+<div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 z-100 flex flex-wrap justify-center gap-2 px-4 md:gap-4 md:bottom-10">
+
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
           onClick={() => handleTextChange("55555555s5555SSSssss555555", "font-arial5")}
