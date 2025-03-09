4786c53485eb56354c27856cfcc132ec09485a1b
gencive5 <vic.segen@gmail.com>
2025-03-09 14:14:02 +0100

    sm00ch in place

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
         </button>
         <button
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={() => handleTextChange("sm00ch", "font-mono")}
+          onClick={() => handleTextChange("sm00ch", "font-sm00ch")}
         >
           sm00ch
         </button>
