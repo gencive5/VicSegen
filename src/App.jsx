32ea19619a69634917c16fba807cd77fd2986d9e
gencive5 <vic.segen@gmail.com>
2025-03-12 22:55:44 +0100

    buttons higher up 4 mobile

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function Home() {
         />
       </div>
 
-      <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 z-100 flex flex-wrap justify-center gap-2 px-4 md:gap-4 md:bottom-18">
+      <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2 z-100 flex flex-wrap justify-center gap-2 px-4 md:gap-4 md:bottom-15">
 
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
