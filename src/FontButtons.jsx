1b3c935ea586047d9b9a4d084360c90697633cbc
gencive5 <vic.segen@gmail.com>
2025-03-31 19:07:09 +0200

    Font buttons oK
    i THinK

--- src/FontButtons.jsx
+++ src/FontButtons.jsx
@@ -2,26 +2,26 @@ const FontButtons = ({ handleButtonClick, handleSm00chClick }) => {
   return (
     <>
       {/* Mobile - Right Column (hidden on desktop) */}
-      <div className="sm:hidden fixed right-0 top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-50 items-end">
+      <div className="sm:hidden fixed right-[-50px] top-1/2 transform -translate-y-1/2 flex flex-col gap-3 z-50 items-end opacity-80">
         <button 
           onClick={() => handleButtonClick("arial5")} 
           className="custom-button bg-arial5-button hover:opacity-90 transition-opacity"
-          style={{ width: '200px', height: '100px' }}
+          style={{ width: '300px', height: '150px' }}
         />
         <button 
           onClick={() => handleButtonClick("triple")} 
           className="custom-button bg-triple-button hover:opacity-90 transition-opacity"
-          style={{ width: '200px', height: '100px' }}
+          style={{ width: '300px', height: '150px' }}
         />
         <button 
           onClick={handleSm00chClick} 
           className="custom-button bg-sm00ch-button hover:opacity-90 transition-opacity"
-          style={{ width: '200px', height: '100px' }}
+          style={{ width: '300px', height: '150px' }}
         />
       </div>
 
       {/* Desktop - Left Column (hidden on mobile) */}
-      <div className="hidden sm:flex fixed left-0 top-1/2 transform -translate-y-1/2 flex-col gap-6 z-50">
+      <div className="hidden sm:flex fixed left-[-50px] top-1/2 transform -translate-y-1/2 flex-col gap-6 z-50">
         <button 
           onClick={() => handleButtonClick("arial5")} 
           className="custom-button bg-arial5-button hover:opacity-90 transition-opacity"
